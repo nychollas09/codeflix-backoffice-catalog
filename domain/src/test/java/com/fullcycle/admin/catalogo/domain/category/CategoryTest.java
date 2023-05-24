db63c74af5b7891b6133067f98205416280038d1
Nichollas Falcão <nychollas2013@hotmail.com>
2023-05-24 18:29:48 -0300

    feat: 🎸 Improve project architecture
    BREAKING CHANGE: 🧨 n
    
    ✅ Closes: n

---
+++ domain/src/test/java/com/fullcycle/admin/catalogo/domain/category/CategoryTest.java
@@ -0,0 +1,24 @@
+package com.fullcycle.admin.catalogo.domain.category;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CategoryTest {
+    @Test
+    public void givenAValidParams_whenCallNewCategory_thenInstantiateACategory() {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        Assertions.assertNotNull(actualCategory);
+        Assertions.assertNotNull(actualCategory.getId());
+        Assertions.assertEquals(actualCategory.getName(), expectedName);
+        Assertions.assertEquals(actualCategory.getDescription(), expectedDescription);
+        Assertions.assertEquals(actualCategory.isActive(), expectedIsActive);
+        Assertions.assertNotNull(actualCategory.getCreatedAt());
+        Assertions.assertNotNull(actualCategory.getUpdatedAt());
+        Assertions.assertNull(actualCategory.getDeletedAt());
+    }
+}
